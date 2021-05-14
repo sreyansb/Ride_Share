346802cdcb1c798625a65e96aa961f82853239d7
revanthbabu <putturevanth@gmail.com>
2021-05-14 10:45:05 +0530

    remove join ride

--- PART1/mainapp.py
+++ PART1/mainapp.py
@@ -1,24 +1,71 @@
-from flask import Flask,request,jsonify,Response
-from flask_restful import Resource,Api
+from flask import Flask, request, jsonify, Response
+from flask_restful import Resource, Api
 import json
 import mysql.connector
 import requests
 
-app=Flask(__name__)
-api=Api(app)
+app = Flask(__name__)
+api = Api(app)
 
-class joinride(Resource):
 
-    def get(self,ride_id):
-        return Response({},status=405,mimetype="application/json")
-    
-    def post(self,ride_id):
-        username=request.args["username"]
+class AddUser(Resource):
+	def put(self):
+		req_json = request.get_json()
+		username = req_json["username"]
+		password = req_json["password"]
+		where = "`username`='"+username+"'"
+		msgbody = {"table": "user", "column": ["username"], "where": where}
+		reply = requests.post(
+			"http://127.0.0.1:5000/api/v1/db/read", json=msgbody)
+		ans = reply.json()
+		if not(ans):
+			return Response({}, status=404, mimetype="application/json")
 
-    
+class RemoveUser(Resource):
+	def delete(self, username):
+		if not(username):
+			return Response({}, status=400, mimetype="application/json")
+		
+		wherecond = "`username`=" + str(username)
+		msgbody = {"table": "ride", "column": ["rideid"], "where": wherecond}
+		reply = requests.post("http://127.0.0.1:5000/api/v1/db/read", json = msgbody)
+		res = reply.json()
+		if res:
+			mydb = mysql.connector.connect(
+				host="localhost", user="root", password="root", database="rideshare")
+			myc = mydb.cursor()
+			query1 = "DELETE FROM ride WHERE rideid=%s"
+			myc.execute(query1, (username,))
+			mydb.commit()
+			query2 = "DELETE FROM rideuser WHERE rideid=%s"
+			myc.execute(query2, (username,))
+			mydb.commit()
+			return Response({}, status=204, mimetype="application/json")
+			# changed status code to 204 because 200 is when message body has stuff
+		else:
+			return Response({}, status=400, mimetype="application/json")
 
-#it is suggested we always provide 'get' method
-class rideapis(Resource):
+
+class CreateRide(Resource):
+	def post(self):
+		req_json = request.get_json()
+		username = created_by = req_json["created_by"]
+		timestamp = req_json["timestamp"]
+		source = req_json["source"]
+		destination = req_json["destination"]
+
+
+class JoinRide(Resource):
+
+	def get(self, ride_id):
+		return Response({}, status=405, mimetype="application/json")
+
+	def post(self, ride_id):
+		username = request.args["username"]
+
+
+# it is suggested we always provide 'get' method
+class RideApis(Resource):
 
     def get(self,ride_id):
         if not(ride_id):
@@ -109,85 +156,81 @@ class rideapis(Resource):
             return Response({},status=400,mimetype="application/json")
 
 
-class writedb(Resource):
+class WriteDB(Resource):
 
-    def get(self):
-        return Response({},status=405,mimetype="application/json")
-    
-    def post(self):
-        mydb=mysql.connector.connect(host="localhost",user="root",password="root",database="rideshare")
-        myc=mydb.cursor()
-        data=request.get_json()
-        #print(data["idiot"])
-        data_insert=data["insert"]
-        data_insert=["'"+i+"'" for i in data_insert]
-        columns=data["column"]
-        columns=["`"+i+"`" for i in columns]
-        tablename=data["table"]
+	def get(self):
+		return Response({}, status=405, mimetype="application/json")
 
-        data_insert=", ".join(data_insert)
-        #columns=",".join(columns)
-        column_names = ', '.join(columns)
-        #placeholders = ', '.join(['%s'] * len(columns))
-        s = f'INSERT INTO `{tablename}` ({column_names}) VALUES ({data_insert});'
-        myc.execute(s)
-        mydb.commit()
+	def post(self):
+		mydb = mysql.connector.connect(
+			host="localhost", user="root", password="root", database="rideshare")
+		myc = mydb.cursor()
+		data = request.get_json()
 
-        myc.close()
-        mydb.close()
+		data_insert = data["insert"]
+		data_insert = ["'"+i+"'" for i in data_insert]
+		columns = data["column"]
+		columns = ["`"+i+"`" for i in columns]
+		tablename = data["table"]
 
-        return Response({},status=204,mimetype="application/json")
+		data_insert = ", ".join(data_insert)
+		# columns=",".join(columns)
+		column_names = ', '.join(columns)
+		# placeholders = ', '.join(['%s'] * len(columns))
+		s = f'INSERT INTO `{tablename}` ({column_names}) VALUES ({data_insert});'
+		myc.execute(s)
+		mydb.commit()
 
-class readdb(Resource):
+		myc.close()
+		mydb.close()
 
-    def get(self):
-        return Response({},status=405,mimetype="application/json")
-    
-    def post(self):
-        mydb=mysql.connector.connect(host="localhost",user="root",password="root",database="rideshare")
-        myc=mydb.cursor()
-        data=request.get_json()
-        
-        tablename=data["table"]
-        colnames=data["column"]
-        columns=["`"+i+"`" for i in colnames]
-        condition=data["where"]
-        columns = ', '.join(columns)
-        #has to be formatted string, cant give normal mysql statements
-        s=f"SELECT {columns} FROM `{tablename}` WHERE {condition};"
-        res=myc.execute(s)
-        #it is myc.fetchone()/fetchall and not res.fetchone()
-        res=myc.fetchall()
+		return Response({}, status=204, mimetype="application/json")
 
-        myc.close()
-        mydb.close()
 
-        final_res=[]
-        n=len(data["column"])
-        for i in res:
-            di={}
-            for j in range(n):
-                di[data["column"][j]]=i[j]
-            final_res.append(di.copy())
-            del di
-        
-        return Response(json.dumps(final_res),status=200,mimetype="application/json")
+class ReadDB(Resource):
 
-class square(Resource):
-    def get(self,num):
-        return jsonify({"answer":num*num,"status_code": 200})
+	def get(self):
+		return Response({}, status=405, mimetype="application/json")
 
-api.add_resource(writedb,"/api/v1/db/write")
+	def post(self):
+		mydb = mysql.connector.connect(
+			host="localhost", user="root", password="root", database="rideshare")
+		myc = mydb.cursor()
+		data = request.get_json()
 
-api.add_resource(readdb,"/api/v1/db/read")
+		tablename = data["table"]
+		colnames = data["column"]
+		columns = ["`"+i+"`" for i in colnames]
+		condition = data["where"]
+		columns = ', '.join(columns)
+		# has to be formatted string, cant give normal mysql statements
+		s = f"SELECT {columns} FROM `{tablename}` WHERE {condition};"
+		res = myc.execute(s)
+		# it is myc.fetchone()/fetchall and not res.fetchone()
+		res = myc.fetchall()
 
-#if ride_id is not given, it sends a 404 error
-api.add_resource(rideapis,"/api/v1/rides/<int:ride_id>")
+		myc.close()
+		mydb.close()
 
-#api.add_resource(Hello,"/")
-api.add_resource(square,"/square/<int:num>")
+		final_res = []
+		n = len(data["column"])
+		for i in res:
+			di = {}
+			for j in range(n):
+				di[data["column"][j]] = i[j]
+			final_res.append(di.copy())
+			del di
 
-if __name__=="__main__":
-    app.run(debug=True)
+		return Response(json.dumps(final_res), status=200, mimetype="application/json")
 
-            
+
+api.add_resource(RemoveUser, "/api/v1/users/<string:username>")
+api.add_resource(AddUser, "/api/v1/users")
+# if ride_id is not given, it sends a 404 error
+api.add_resource(RideApis, "/api/v1/rides/<int:ride_id>")
+api.add_resource(CreateRide, "/api/v1/rides")
+api.add_resource(WriteDB, "/api/v1/db/write")
+api.add_resource(ReadDB, "/api/v1/db/read")
+
+if __name__ == "__main__":
+	app.run(debug=True)
