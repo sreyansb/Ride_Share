978877f7cdb4f75a5ba861bb04dcbb0c19e1abe8
revanthbabu <putturevanth@gmail.com>
2021-05-14 12:19:34 +0530

    fix delete in RideApis
    first delete from rideuser then from ride.

--- PART1/mainapp.py
+++ PART1/mainapp.py
@@ -88,93 +88,93 @@ class CreateRide(Resource):
 # it is suggested we always provide 'get' method
 class RideApis(Resource):
 
-    def get(self,ride_id):
-        if not(ride_id):
-            return Response({},status=400,mimetype="application/json")
-        
-        #to verify that the ride exists
-        wherecond="`rideid`="+str(ride_id)
-        msgbody={"table":"ride","column":["rideid"],"where":wherecond}
-        reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
-        ans=reply.json()
-        if not(ans):
-            return Response({},status=404,mimetype="application/json")
-        
-        #to get the rideID, created_by, Timestamp, source and destination of the ride
-        msgbody={"table":"ride","column":["rideid","created_by","source","destination","timestamp"],"where":wherecond}
-        reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
-        ans1=reply.json()
+	def get(self,ride_id):
+		if not(ride_id):
+			return Response({},status=400,mimetype="application/json")
+		
+		#to verify that the ride exists
+		wherecond="`rideid`="+str(ride_id)
+		msgbody={"table":"ride","column":["rideid"],"where":wherecond}
+		reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
+		ans=reply.json()
+		if not(ans):
+			return Response({},status=404,mimetype="application/json")
+		
+		#to get the rideID, created_by, Timestamp, source and destination of the ride
+		msgbody={"table":"ride","column":["rideid","created_by","source","destination","timestamp"],"where":wherecond}
+		reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
+		ans1=reply.json()
 
-        #to get all the users of the ride
-        msgbody={"table":"rideuser","column":["username"],"where":wherecond}
-        reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
-        ans2=reply.json()
+		#to get all the users of the ride
+		msgbody={"table":"rideuser","column":["username"],"where":wherecond}
+		reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
+		ans2=reply.json()
 
-        di={}
-        di["rideId"]=ans1[0]["rideid"]
-        di["Created_By"]=ans1[0]["created_by"]
-        di["Timestamp"]=ans1[0]["timestamp"]
-        di["source"]=ans1[0]["source"]
-        di["destination"]=ans1[0]["destination"]
-        di["users"]=[]
-        for i in ans2:
-            di["users"].append(i["username"])
-    
-        return Response(json.dumps(di),status=200,mimetype="application/json")
-    
-    def post(self,ride_id):
-        if not(ride_id):
-            return Response({},status=400,mimetype="application/json")
-        
-        #to check if ride exists
-        wherecond="`rideid`="+str(ride_id)
-        msgbody={"table":"ride","column":["rideid"],"where":wherecond}
-        reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
-        ans=reply.json()
-        if not(ans):
-            return Response({},status=404,mimetype="application/json")
-        
-        #to check if user_name exists in the body of the message
-        username=request.get_json()["username"]
-        wherecond="`username`='"+username+"'"# have to give quotes around username or string types
-        msgbody={"table":"user","column":["username"],"where":wherecond}
-        reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
-        ans=reply.json()
-        if not(ans):
-            return Response({},status=404,mimetype="application/json")
-        
-        wherecond="`username`='"+username+"' AND "+"`rideid`="+str(ride_id)
-        msgbody={"table":"rideuser","column":["rideid","username"],"where":wherecond}
-        reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
-        ans=reply.json()
-        if ans:
-            #if already found=>it was already there => bad request
-            return Response({},status=400,mimetype="application/json")
-        else:
-            msgbody={"table":"rideuser","column":["rideid","username"],"insert":[ride_id,username]}
-            reply=requests.post("http://127.0.0.1:5000/api/v1/db/write",json=msgbody)
-    
-    def delete(self,ride_id):
-        #return Response({},status=400,mimetype="application/json")
-        if not(ride_id):
-            return Response({},status=400,mimetype="application/json")
-        wherecond="`rideid`="+str(ride_id)
-        msgbody={"table":"ride","column":["rideid"],"where":wherecond}
-        reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
-        res=reply.json()
-        if res:
-            mydb=mysql.connector.connect(host="localhost",user="root", database="rideshare")
-            myc=mydb.cursor()
-            query1 = "DELETE FROM ride WHERE rideid=%s"
-            myc.execute(query1,(ride_id,))
-            mydb.commit()
-            query2 = "DELETE FROM rideuser WHERE rideid=%s"
-            myc.execute(query2,(ride_id,))
-            mydb.commit()
-            return Response({},status=204,mimetype="application/json")
-            #changed status code to 204 because 200 is when message body has stuff
-        else:
-            return Response({},status=400,mimetype="application/json")
+		di={}
+		di["rideId"]=ans1[0]["rideid"]
+		di["Created_By"]=ans1[0]["created_by"]
+		di["Timestamp"]=ans1[0]["timestamp"]
+		di["source"]=ans1[0]["source"]
+		di["destination"]=ans1[0]["destination"]
+		di["users"]=[]
+		for i in ans2:
+			di["users"].append(i["username"])
+	
+		return Response(json.dumps(di),status=200,mimetype="application/json")
+	
+	def post(self,ride_id):
+		if not(ride_id):
+			return Response({},status=400,mimetype="application/json")
+		
+		#to check if ride exists
+		wherecond="`rideid`="+str(ride_id)
+		msgbody={"table":"ride","column":["rideid"],"where":wherecond}
+		reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
+		ans=reply.json()
+		if not(ans):
+			return Response({},status=404,mimetype="application/json")
+		
+		#to check if user_name exists in the body of the message
+		username=request.get_json()["username"]
+		wherecond="`username`='"+username+"'"# have to give quotes around username or string types
+		msgbody={"table":"user","column":["username"],"where":wherecond}
+		reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
+		ans=reply.json()
+		if not(ans):
+			return Response({},status=404,mimetype="application/json")
+		
+		wherecond="`username`='"+username+"' AND "+"`rideid`="+str(ride_id)
+		msgbody={"table":"rideuser","column":["rideid","username"],"where":wherecond}
+		reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
+		ans=reply.json()
+		if ans:
+			#if already found=>it was already there => bad request
+			return Response({},status=400,mimetype="application/json")
+		else:
+			msgbody={"table":"rideuser","column":["rideid","username"],"insert":[ride_id,username]}
+			reply=requests.post("http://127.0.0.1:5000/api/v1/db/write",json=msgbody)
+	
+	def delete(self,ride_id):
+		#return Response({},status=400,mimetype="application/json")
+		if not(ride_id):
+			return Response({},status=400,mimetype="application/json")
+		wherecond="`rideid`="+str(ride_id)
+		msgbody={"table":"ride","column":["rideid"],"where":wherecond}
+		reply=requests.post("http://127.0.0.1:5000/api/v1/db/read",json=msgbody)
+		res=reply.json()
+		if res:
+			mydb=mysql.connector.connect(host="localhost",user="root", database="rideshare")
+			myc=mydb.cursor()
+			query2 = "DELETE FROM rideuser WHERE rideid=%s"
+			myc.execute(query2,(ride_id,))
+			mydb.commit()
+			query1 = "DELETE FROM ride WHERE rideid=%s"
+			myc.execute(query1,(ride_id,))
+			mydb.commit()
+			return Response({},status=204,mimetype="application/json")
+			#changed status code to 204 because 200 is when message body has stuff
+		else:
+			return Response({},status=400,mimetype="application/json")
 
 
 class WriteDB(Resource):
